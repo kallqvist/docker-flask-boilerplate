e339000019a0e07900757a28572f9b8e3c1f5d6c
klas <kallqvist@gmail.com>
2018-06-27 20:37:07 +0200

    fix broken stuff with newer pip versions in base image

--- code/backend/setup/setup.py
+++ code/backend/setup/setup.py
@@ -1,7 +1,11 @@
 import os
 from setuptools import setup
-from pip.req import parse_requirements
-from pip.download import PipSession
+try: # pip >= 10.0
+    from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError: # pip <= 9.0.3
+    from pip.req import parse_requirements
+    from pip.download import PipSession
 
 req_file = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 reqs = [str(r.req) for r in parse_requirements(req_file, session=PipSession())]
